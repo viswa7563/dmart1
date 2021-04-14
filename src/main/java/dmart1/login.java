90a6424bb514ce838a82195f7984041259352316
root <root@ip-172-31-26-175.us-east-2.compute.internal>
2021-04-14 03:46:13 +0000

    modifed

---
+++ src/main/java/dmart1/login.java
@@ -0,0 +1,13 @@
+package dmart1;
+
+/**
+ * Hello world!
+ *
+ */
+public class login
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
